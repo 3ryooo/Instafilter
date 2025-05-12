cc83e5e6c0e9905d2ecc8ae54987af4c6ba6d8b6
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-12 11:25:14 +0900

    finish: https://www.hackingwithswift.com/books/ios-swiftui/building-our-basic-ui

--- Instafilter/View/ContentView.swift
+++ Instafilter/View/ContentView.swift
@@ -4,18 +4,56 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
+    
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+
+                    
+                    Spacer()
+                    
+                    // share the picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        .padding()
+
     }
+    
+    func changeFilter() {
+    }
+
+    
 }
+
 
 #Preview {
     ContentView()
